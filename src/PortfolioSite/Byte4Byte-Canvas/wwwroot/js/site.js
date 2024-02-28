e1c64543f20d8de008dc2316f50d2af9e11c5f5a
Bubbles0989 <jasmith17@mail.wou.edu>
2024-02-27 16:58:16 -0800

    revamped home page, generated wireframes for the pages and added them to docu

--- src/PortfolioSite/Byte4Byte-Canvas/wwwroot/js/site.js
+++ src/PortfolioSite/Byte4Byte-Canvas/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+$(document).ready(function () {
+
+  window.onscroll = function() {stickyNavbar()};
+
+  var navbar = document.getElementById("navbar");
+  var sticky = navbar.offsetTop;
+
+  function stickyNavbar() {
+    if (window.scrollY >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }
+
+});

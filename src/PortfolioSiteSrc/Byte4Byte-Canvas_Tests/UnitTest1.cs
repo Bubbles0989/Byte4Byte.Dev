5876d0bce4ba5210d28a5c9910f2518449c5276c
Bubbles0989 <jasmith17@mail.wou.edu>
2024-12-03 12:18:00 -0800

    site readjustments

---
+++ src/PortfolioSiteSrc/Byte4Byte-Canvas_Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Byte4Byte_Canvas_Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ src/PortfolioSiteSrc/PortfolioSite/Byte4Byte-Canvas/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+$(document).ready(function () {
+
+  window.onscroll = function() {stickyNavbar()};
+
+  var navbar = document.getElementById("navbar");
+  var sticky = navbar.offsetTop;
+
+  function stickyNavbar() {
+    if (window.scrollY >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }
+});

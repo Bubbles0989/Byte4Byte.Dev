795f1297d05aa2193434f34b9fc676c431bfe675
Bubbles0989 <jasmith17@mail.wou.edu>
2025-01-13 17:51:53 -0800

    readjusted the gifs some more :3

--- src/PortfolioSiteSrc/PortfolioSite/Byte4Byte-Canvas/obj/Debug/net7.0/Byte4Byte-Canvas.AssemblyInfo.cs
+++ src/PortfolioSiteSrc/PortfolioSite/Byte4Byte-Canvas/obj/Debug/net7.0/Byte4Byte-Canvas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Byte4Byte-Canvas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f6a84889349239b0d7d439e49e101b822fc5c6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3edd9c026e67a1f731377acc4fd0c51c6db70e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Byte4Byte-Canvas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Byte4Byte-Canvas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

2ae16b898d0203f2374eb545e86ba6bb04e0ba3e
Bubbles0989 <jasmith17@mail.wou.edu>
2025-02-24 12:11:33 -0800

    moved new model into gallery

--- src/PortfolioSiteSrc/PortfolioSite/Byte4Byte-Canvas/obj/Debug/net7.0/Byte4Byte-Canvas.AssemblyInfo.cs
+++ src/PortfolioSiteSrc/PortfolioSite/Byte4Byte-Canvas/obj/Debug/net7.0/Byte4Byte-Canvas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Byte4Byte-Canvas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d22a3a397d97c5ea5c8d4cc09899b3bcd05c368")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+babd2a2820de6f06d8b5ff6ed3bc6fa2da6b12f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Byte4Byte-Canvas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Byte4Byte-Canvas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
